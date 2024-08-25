9b85a9072ecc0d4a5c7107685679f0a18f240bdb
yba9285 <108516941+yba9285@users.noreply.github.com>
2024-08-25 09:44:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DB_NAME = os.environ.get("DB_NAME", "mzfilestore02")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002161495673"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002148911492 -1001520014377"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002148911492"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
